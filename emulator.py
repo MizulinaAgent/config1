80ec73948e8d17260f0e10581d40badc5b0253e3
MizulinaAgent <mikniksertan@gmail.com>
2024-11-14 00:26:24 +0300

    Revert "Сделали функцию для строчки ввода"
    This reverts commit c4e4886e5469464db66b6377d3225b186457768a.

--- emulator.py
+++
@@ -1,7 +0,0 @@
-def prompt(username, current_path):
-    home_path = "/root"
-    if current_path == home_path:
-        path_display = "~"
-    else:
-        path_display = current_path.lstrip('/')
-    return f"{username}_emu:{path_display}$ "
