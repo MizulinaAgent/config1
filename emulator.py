c4e4886e5469464db66b6377d3225b186457768a
MizulinaAgent <mikniksertan@gmail.com>
2024-11-13 23:56:50 +0300

    Сделали функцию для строчки ввода

---
+++ emulator.py
@@ -0,0 +1,7 @@
+def prompt(username, current_path):
+    home_path = "/root"
+    if current_path == home_path:
+        path_display = "~"
+    else:
+        path_display = current_path.lstrip('/')
+    return f"{username}_emu:{path_display}$ "
