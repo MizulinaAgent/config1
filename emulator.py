e945f8bdb0a92e4bd10711371d9c3057a55a56d7
MizulinaAgent <mikniksertan@gmail.com>
2024-11-14 00:31:34 +0300

    Добавили парс аргументы

--- emulator.py
+++ emulator.py
@@ -1,3 +1,6 @@
+import argparse
+
+
 def prompt(username, current_path):
     home_path = "/root"
     if current_path == home_path:
@@ -5,3 +8,9 @@ def prompt(username, current_path):
     else:
         path_display = current_path.lstrip('/')
     return f"{username}_emu:{path_display}$ "
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Эмулятор оболочки")
+    parser.add_argument('--user', required=True, help='Имя пользователя для приглашения')
+    parser.add_argument('--zip', required=True, help='Путь к zip-файлу с виртуальной файловой системой')
+    return parser.parse_args()
